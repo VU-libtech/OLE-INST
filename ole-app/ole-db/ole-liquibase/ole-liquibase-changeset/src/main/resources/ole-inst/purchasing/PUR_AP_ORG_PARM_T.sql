906ffc57ddfcc724452a88850c6e845a252f6768
dlacy <david.lacy@villanova.edu>
2014-10-21 10:48:38 -0400

    Added Penn's vendor and GL data

---
+++ ole-app/ole-db/ole-liquibase/ole-liquibase-changeset/src/main/resources/ole-inst/purchasing/PUR_AP_ORG_PARM_T.sql
@@ -0,0 +1,5 @@
+/*
+-- Query: SELECT * FROM ole.pur_ap_org_parm_t
+-- Date: 2014-10-21 09:05
+*/
+INSERT INTO `pur_ap_org_parm_t` (`FIN_COA_CD`,`ORG_CD`,`OBJ_ID`,`VER_NBR`,`ORG_AUTO_PO_LMT`,`ACTV_IND`) VALUES ('OU','DEFA','ee0059d4-d177-4a64-bf93-f595e992abc7',1,10000.00,'Y');
