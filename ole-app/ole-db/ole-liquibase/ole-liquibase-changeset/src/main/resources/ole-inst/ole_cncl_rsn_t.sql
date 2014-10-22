bdc082b05951b567650a79fb41ebceed511ea573
dlacy <david.lacy@villanova.edu>
2014-10-22 11:27:36 -0400

    Added staff affiliate and associated attributes. Added cancellation reasons.

---
+++ ole-app/ole-db/ole-liquibase/ole-liquibase-changeset/src/main/resources/ole-inst/ole_cncl_rsn_t.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM ole.ole_cncl_rsn_t
+-- Date: 2014-10-22 08:04
+*/
+INSERT INTO `ole_cncl_rsn_t` (`CNCL_RSN_ID`,`CNCL_RSN_NM`,`CNCL_RSN_TXT`,`ROW_ACT_IND`,`OBJ_ID`,`VER_NBR`) VALUES ('6','Wrong item','Wrong item sent','Y','678eedca-48a3-4c24-a1b6-2f86bede728b',1);
+INSERT INTO `ole_cncl_rsn_t` (`CNCL_RSN_ID`,`CNCL_RSN_NM`,`CNCL_RSN_TXT`,`ROW_ACT_IND`,`OBJ_ID`,`VER_NBR`) VALUES ('7','No longer needed','No longer needed','Y','60aa92db-8b99-435a-a9f1-1e5e0b3a6f92',1);
+INSERT INTO `ole_cncl_rsn_t` (`CNCL_RSN_ID`,`CNCL_RSN_NM`,`CNCL_RSN_TXT`,`ROW_ACT_IND`,`OBJ_ID`,`VER_NBR`) VALUES ('8','Obtained from another source','Obtained from another source','Y','27937fdc-b793-4a18-b10c-09d0e8f3cc78',1);
