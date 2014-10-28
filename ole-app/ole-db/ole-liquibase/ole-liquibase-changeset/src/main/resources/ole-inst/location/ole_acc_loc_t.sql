d364c7af91a070827a721e3b88b2632a578daaf2
dlacy <david.lacy@villanova.edu>
2014-10-28 10:12:42 -0400

    overly OU data

---
+++ ole-app/ole-db/ole-liquibase/ole-liquibase-changeset/src/main/resources/ole-inst/location/ole_acc_loc_t.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM ole.ole_acc_loc_t
+LIMIT 0, 1000
+
+-- Date: 2014-09-24 09:14
+*/
+INSERT INTO `ole_acc_loc_t` (`ACC_LOC_ID`,`OBJ_ID`,`VER_NBR`,`ACC_LOC_NM`,`ACC_LOC_DESC`,`ROW_ACT_IND`) VALUES ('1','bdd811ce-9cb5-463b-8032-c8bdc9d1542f',1,'Main Campus','Main Campus','Y');
