e4a084f16e3757616975b03b02d149622c64fb31
David Lacy <david.lacy@villanova.edu>
2015-03-06 14:33:36 -0500

    starting over

--- ole-app/olefs/src/main/java/org/kuali/ole/select/testing/package-info.java
+++
@@ -1,1 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.select.ole.kuali.org/") package org.kuali.ole.select.testing;
--- ole-app/olefs/src/main/webapp/ole/select/scripts/donor.js
+++
@@ -1,9 +0,0 @@
-jq(document).ready(function(){
-    jq("#donorName_control").attr('maxlength','255');
-});
-jq(document).click(function(){
-    jq("#donorName_control").attr('maxlength','255');
-});
-jq(document).keypress(function(){
-    jq("#donorName_control").attr('maxlength','255');
-});
